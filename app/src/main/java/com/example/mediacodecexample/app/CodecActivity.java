e292f853a61592d35e50d865eee0be6e070bfcd2
christianvazquez7 <christian.vazquez7@upr.edu>
2014-07-31 10:42:55 -0400

    video recording with a mediaplayer

--- app/src/main/java/com/example/mediacodecexample/app/CodecActivity.java
+++ app/src/main/java/com/example/mediacodecexample/app/CodecActivity.java
@@ -1,6 +1,8 @@
 package com.example.mediacodecexample.app;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -8,6 +10,8 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
+
+import java.io.File;
 
 
 public class CodecActivity extends Activity implements SurfaceHolder.Callback {
@@ -17,15 +21,44 @@ public class CodecActivity extends Activity implements SurfaceHolder.Callback {
     private final static String TAG = "CodecActivity";
     private TakeTwoMediaRecorder customRecorder;
     private Handler handler = new Handler();
+    private SurfaceView videoView;
+    private MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_codec);
+        mp = MediaPlayer.create(this, Uri.fromFile(new File("/sdcard/merge.mp4")));
         this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         frame = (FrameLayout) this.findViewById(R.id.camera_preview);
         surfaceView = (SurfaceView) this.findViewById(R.id.camera_surface);
         surfaceView.getHolder().addCallback(this);
+        videoView = (SurfaceView) this.findViewById(R.id.video_surface);
+        mp.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.start();
+            }
+        });
+        videoView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                surfaceHolder.setFixedSize(320, 240);
+                surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+                mp.setDisplay(surfaceHolder);
+                //mp.prepareAsync();
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i2, int i3) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+            }
+        });
         SurfaceView dummy = new SurfaceView(this);
         frame.addView(dummy);
     }
@@ -62,7 +95,7 @@ public class CodecActivity extends Activity implements SurfaceHolder.Callback {
             public void run() {
                 customRecorder.stop(false);
             }
-        }, 20000);
+        }, 10000);
 
     }
 
